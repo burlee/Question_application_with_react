22ca9fdcf34c3842735e125b4a253b2170574b64
Seweryn Kimla <sewkim@vp.pl>
2018-07-30 20:16:10 +0200

    Added tryAgain button

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import questionPackage from '../../Data/Questions.js'
+
+import questionPackage from '../../Data/Questions.js';
 import Aux from '../../HOC/aux_x';
 import Hint from '../Hint/Hint';
 import classes from './Quiz.css';
@@ -8,22 +9,18 @@ export default class Quiz extends Component {
     state = {
         questions: questionPackage,
         points: 0,
-        badAnswers: 0,
         startSlice: 0,
         endSlice: 1,
         currentClickedBtnID: '',
         badAnswer: false
     }
     
-    validationAnswer = (clickedBtn) => {
-        console.log("Kliknięty : " + clickedBtn);
-        
+    validationAnswer = (clickedBtn) => {        
         const previousCorrectlyAnswerValue = this.state.points + 1;
         const startSlice = this.state.startSlice;
         const endSlice = this.state.endSlice;
         this.setState({
             points: previousCorrectlyAnswerValue,
-            clicked: true,
             currentClickedBtnID: clickedBtn,
             startSlice: startSlice + 1,
             endSlice: endSlice + 1
@@ -38,9 +35,10 @@ export default class Quiz extends Component {
         });
         setTimeout(() => this.setState({badAnswer: false}), 1250);
     }
-
+    pageReload = () => {
+        window.location.reload();
+    }
     render() {
-        console.log(this.state.questions)
         let questions = this.state.questions.questions
         .slice(this.state.startSlice , this.state.endSlice)
         .map( question => {
@@ -97,6 +95,7 @@ export default class Quiz extends Component {
                 return (
                     <div className={classes.QuestionBoxEnd} key={question.id}>
                         <h5>{question.testEndMessage}{this.state.points - this.state.questions.questions.length + 1} błędnych odpowiedzi na {this.state.questions.questions.length} pytań.</h5>
+                        <button onClick={this.pageReload}>Spróbuj ponownie</button>
                     </div>
                 )
             }
